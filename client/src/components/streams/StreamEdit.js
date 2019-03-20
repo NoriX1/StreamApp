68528b8577092ab3890f639751f9b5565bcea787
NoriX <versatility.95@mail.ru>
2019-03-20 17:58:44 +0300

    Issue with loading component data in case with opening link directly

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
+const StreamEdit = (props) => {
+    console.log(props)
     return <div>StreamEdit</div>;
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+        // With React-Router each component needs to be designed
+        // to work in isolation (fetch its own data!)
+        // So now, if we will open link directly
+        // We can`t receive this stream data
+        // Because StreamList component don`t loaded it yet
+    }
+}
+
+export default connect(mapStateToProps)(StreamEdit);
