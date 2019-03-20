f1c15eab6d966150d3856a608744967c3831f98f
NoriX <versatility.95@mail.ru>
2019-03-20 14:49:07 +0300

    Edit Delete and Create stream buttons

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
 
@@ -8,10 +9,22 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+            );
+        }
+    }
+
     renderList() {
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera"></i>
                     <div className="content">
                         <div>{stream.title}</div>
@@ -22,11 +35,22 @@ class StreamList extends React.Component {
         })
     }
 
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/streams/new" className="ui button primary">Create Stream</Link>
+                </div>
+            );
+        }
+    }
+
     render() {
         return (
             <div>
                 <h2>Streams</h2>
                 <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
         );
     }
@@ -34,7 +58,11 @@ class StreamList extends React.Component {
 
 const mapStateToProps = state => {
     //Object.values will insert object values into array
-    return { streams: Object.values(state.streams) };
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    };
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
