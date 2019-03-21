3e2c5318e4d973d6a7d8c154912800d7f89e4670
NoriX <versatility.95@mail.ru>
2019-03-21 16:19:37 +0300

    StreamShow Component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -27,7 +27,7 @@ class StreamList extends React.Component {
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera"></i>
                     <div className="content">
-                        <div>{stream.title}</div>
+                        <Link to={`/streams/${stream.id}`} className="header">{stream.title}</Link>
                         <div className="description">{stream.description}</div>
                     </div>
                 </div>
